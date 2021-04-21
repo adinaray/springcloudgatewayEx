a2f80b3d985e7a467e3d9ace83b0de66e76ccd7b
adi <adinarayanatangella@gmail.com>
2021-04-21 22:31:13 +0530

    Initial commit

---
+++ src/test/java/com/example/springcloudgatewayEx/SpringcloudgatewayExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcloudgatewayEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudgatewayExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
